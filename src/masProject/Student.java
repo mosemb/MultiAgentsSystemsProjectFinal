aa64496ee419fb2b989a2c6e1aa9216777e62dd8
mosemb <mosejava@gmail.com>
2021-03-08 10:12:56 +0100

    Working Message flow Student Committee

--- src/masProject/Student.java
+++ src/masProject/Student.java
@@ -1,88 +1,164 @@
 package masProject;
 
+
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.FIPANames.MTP;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
 
 public class Student extends Agent{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	boolean enrollDesertation; 
 	boolean examinProposals; 
+	MessageTemplate mtStudentCommt;
 	ThesisCommt thesisCommt;
 	Object [] args;
 	
 	public void setup() { 
 		
 		args = getArguments(); 
+		String choice = (String) args[0];
+		//String [] choices  = {"Company", "Proposals", "StudentChoice"};
 		
-		System.out.println(getAID().getName());
-		addBehaviour(new choiceDesertation());
+		//for(String x:choices) {
+			
+			
+				
+				
+			switch (choice) {
+			case "Company":
+				System.out.println("Chose a company");
+				System.out.println(getAID().getName());
+				addBehaviour(new choiceDesertation());
+				addBehaviour(new RecievedMsgThCom());
+				break;
+				
+			case "Proposals":
+				System.out.println("Chose from Proposals");
+				break;
+				
+			case "StudentChoice":
+				System.out.println("Its a Student Choice");
+				break;
+				
+
+			default:
+				System.out.println("Not a valid Choice");
+				break;
+			}	
+
+			
 		
 	}
 	
 	// Put agent clean-up operations here
-		protected void takeDown() {
+	protected void takeDown() {
 			// Printout a dismissal message
 			System.out.println("Student-agent "+getAID().getName()+" terminating.");
 		}
 	
 	public class choiceDesertation extends Behaviour {
 		
-		String choice = (String) args[0];
-		String [] choices  = {"Company", "Proposals", "StudentChoice"};
+		/**
+		 * 
+		 */
+
+		int step = 0; 
 		public void action() {
 			
+					
+					switch (step) {
+					case 0:
+						System.out.println("Chose a company");
+						ACLMessage pr = new ACLMessage(ACLMessage.PROPOSE); 
+						pr.addReceiver(new AID( "commt",AID.ISLOCALNAME));
+						pr.setConversationId("TChoice-Company");
+						pr.setReplyWith("Proposal "+ System.currentTimeMillis());
+						pr.setContent("Company Thesis");
+						send(pr);
+						
+						// Prepare a reply from the proposals 
+						mtStudentCommt = MessageTemplate.and(MessageTemplate.MatchConversationId("TChoice-Company"), 
+								MessageTemplate.MatchInReplyTo(pr.getReplyWith()));
+						step =1;
+						System.out.println(pr);
+						break;
+					//case 1:
+						 /*ACLMessage reply = myAgent.receive(mt);
+						 //System.out.println("The MT"+mt);
+						 //System.out.println("The Reply"+reply);
+						 
+						 
+						 //ACLMessage reply = myAgent.receive(mt);
+							
+							if(reply!=null) {
+								String cont = reply.getContent();
+								
+								if(reply.getPerformative()==ACLMessage.ACCEPT_PROPOSAL) {
+									System.out.println("Accepted");
+									System.out.println(cont); 
+									
+								}else if (reply.getPerformative()==ACLMessage.REJECT_PROPOSAL) {
+									System.out.println("Rejected");
+									System.out.println(cont);
+								}
+								
+								 
+							}else {
+								block();
+							}*
+							
+							//System.out.println(mt);
+							//System.out.println("Test");
+							step = 2;
+							//System.out.println("Step no:"+step);
+							break;*/
+						}
+							
+			}
+		
+		@Override
+		public boolean done() {
+			// TODO Auto-generated method stub
+			return step==2;
+		} 
 			
-			for(String x: choices) {
+		}
+	
+	public class RecievedMsgThCom extends CyclicBehaviour{
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+			//mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = myAgent.receive(mtStudentCommt);
+			
+			if(msg!=null) {
 				
-				if(choice.equals(x)) {
-					choice = x;
-					break;}
+				if(msg.getConversationId().equals("TChoice-Company")) {
+					String content = msg.getContent();
+					System.out.println(content);
+				}
+				
+			}else {
+				block();
 			}
 			
-			
-				switch (choice) {
-				
-				case "Company":
-					System.out.println("Chose a company");
-					ACLMessage pr = new ACLMessage(ACLMessage.PROPOSE); 
-					pr.addReceiver(new AID( "commtee",AID.ISLOCALNAME));
-					pr.setConversationId("Thesis-Choice-Company");
-					pr.setReplyWith("Proposal "+ System.currentTimeMillis());
-					pr.setContent("Company Thesis");
-					send(pr);
-					System.out.print(pr);
-					
-					break;
-					
-				case "Proposals":
-					System.out.println("Chose a proposal"); 
-					break;
-					
-				case "StudentChoice":
-					System.out.println("Chose a student choice");
-					break;
-				default:
-					System.out.println("Choice not found");
-					break;
-				
-			} 
-			
-			
-			
 		}
 		
-		
-		public boolean done() {
+	}
 			
-			return choice.equals((String)args[0]);
-		}
-		
-	} 
+	}
 	
 	
 	
 
-}
+
--- src/masProject/ThesisCommt.java
+++ src/masProject/ThesisCommt.java
@@ -1,41 +1,75 @@
 package masProject;
 
+
 import jade.core.AID;
+//import jade.core.AID;
 import jade.core.Agent;
+//import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 public class ThesisCommt extends Agent{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void setup() {
 		
 		
           System.out.println("Agent "+getLocalName()+" is ready. ");
-          //addBehaviour(b);
-		
-		   addBehaviour(new CyclicBehaviour(this) {
-			public void action() {
+          
+          
+          addBehaviour(new TickerBehaviour(this,2000) {
+			
+			@Override
+			protected void onTick() {
+				// TODO Auto-generated method stub
+				myAgent.addBehaviour(new  recieveFromStudent());
 				
-				//ACLMessage msg = myAgent.receive();
-				//if (msg != null) {
-				// Message received. Process it
+			}
+		});
+          
+    }
+		  
+	
+	public class recieveFromStudent extends  CyclicBehaviour {
+			public void action() {
 				
 				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.PROPOSE);
 				ACLMessage msg = receive(mt);
 				
 				if (msg != null) {
 					String title = msg.getContent();
+					boolean acceptable = false; 
 					System.out.println("Message matching custom template received:");
+					ACLMessage reply = msg.createReply();
 					System.out.println(title);
-					//System.out.println(title);
+					
+					
+					if(acceptable==true) {
+						reply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+						reply.setContent("PROPOSAL ACCEPTED"); 
+					
+						
+					}else {
+						reply.setPerformative(ACLMessage.REJECT_PROPOSAL);
+						reply.setContent("PROPOSAL REJECTED");
+			
+					}
+					myAgent.send(reply);
+					
 				}
 				else {
 					block();
 				}
 			}
-		} );
-		
-	}
+		}
+	
+	
+	
 	
 }
