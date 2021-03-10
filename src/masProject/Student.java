acfff9831098abba10026549c369d6a4e9321711
mosemb <mosejava@gmail.com>
2021-03-10 10:08:53 +0100

    Done Supervisor Communication

--- src/masProject/Student.java
+++ src/masProject/Student.java
@@ -1,28 +1,42 @@
 package masProject;
 
 
-
+import java.util.*;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.FIPAException;
 import jade.domain.FIPANames.MTP;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 
+
 public class Student extends Agent{
+	
+	Supervisor stSupervisor = new Supervisor();
+	ArrayList<String> tst = stSupervisor.getThesisProposaList();
+
+	
+	
+
 	
 	/**
 	 * 
 	 */
+	boolean interested = true;
+	int randomNo;
 	private static final long serialVersionUID = 1L;
 	boolean enrollDesertation; 
 	boolean examinProposals; 
 	boolean selectProposal;
 	MessageTemplate mtStudentCommt, mtStudentSupp;
-	
+	Supervisor spSupervisor;
 	ThesisCommt thesisCommt;
+
+	
 	Object [] args;
 	
 	public void setup() { 
@@ -39,22 +53,30 @@ public class Student extends Agent{
 			
 		}
 		
+		
+		
 			switch (choice) {
 			case "Company":
 				System.out.println("Student chose to do Thesis with  a company");
 				//System.out.println(getAID().getName());
 				addBehaviour(new choiceDesertation());
 				addBehaviour(new RecievedMsgThCom());
+				
+				
 				break;
 				
 			case "Proposals":
 				System.out.println("Student chose from proposals");
 				addBehaviour(new  StudentProposal());  
 				addBehaviour(new ThesisInfoFromSup());
+				addBehaviour(new RecieveNumOfThesis());
+				//addBehaviour(new InterestedProposal());
+				
 				break;
 				
 			case "StudentChoice":
 				System.out.println("Its a Student Choice");
+				//addBehavior(new test());
 				break;
 				
 
@@ -129,7 +151,7 @@ public class Student extends Agent{
 	public class StudentProposal extends Behaviour{
 		int num =0; 
 		public void action() {
-			 selectProposal = true;
+			 selectProposal = false;
 			 
 			 switch (num) {
 			case 0:
@@ -165,25 +187,106 @@ public class Student extends Agent{
 			// TODO Auto-generated method stub
 			ACLMessage msg = myAgent.receive(mtStudentSupp);
 			
+			
 			if(msg!=null) {
 				
 				String content = msg.getContent();
 				System.out.println();
 				System.out.println("Reply from Supervisor to Student ...");
 				System.out.println(content);
+				//System.out.println("Array size :"+tst.size());
+				interested =true;
+				ACLMessage reply = msg.createReply();
+				//System.out.println(reply);
+				if(interested==true) {
 				
+					reply.setPerformative(ACLMessage.CONFIRM);
+					String string = String.valueOf(randomNo);
+					
+					reply.setContent("THESIS_SELECTED");
+					reply.setConversationId("TChoice-Proposal1");
+				}else {
+					
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("THESIS NOT SELECTED");
+					reply.setConversationId("TChoice-Proposal1");
+				}
+				myAgent.send(reply);
 				
 			}else {
 				block();
+			}	
+		}
+		
+	}
+	
+	public class RecieveNumOfThesis extends CyclicBehaviour{
+
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+			
+			MessageTemplate mT = MessageTemplate.MatchConversationId("numberOfThesis");
+			
+			ACLMessage thNo = receive(mT); 
+			if(thNo!=null) {
+				String content = thNo.getContent();
+				
+			System.out.println("Number of possible thesis available : "+content); 	
+			//System.out.println(content);
+			Random random = new Random(); 
+			randomNo = random.nextInt((Integer.parseInt(content)-1)-0)+0;
+			//System.out.println("Random number is "+randomNo); 
+				
+			}else {
+				
+				block();
 			}
+			
+		} }
+		
+public class InterestedProposal extends Behaviour{
+
+			@Override
+			public void action() {
+				// TODO Auto-generated method stub
+				int step = 0;
+				switch (step) {
+				case 0:
+					if(interested==true && step ==0) {
+						
+						ACLMessage pr = new ACLMessage(ACLMessage.INFORM_REF); 
+						pr.addReceiver(new AID( "supervisor",AID.ISLOCALNAME));
+						pr.setConversationId("chosenProposalNo");
+						pr.setReplyWith("Inform_ref "+ System.currentTimeMillis());
+						String string = String.valueOf(randomNo);
+						pr.setContent(string);
+						send(pr);
+						step =2;
+						//interested=false;
+					}
+					break;
+
+		
+				} }
+
+			@Override
+			public boolean done() {
+				// TODO Auto-generated method stub
+				return true;
+			}
+				
+				
+			}
+
 			
 		}
 		
 		
-	}
+
+
 	
 	
-	}
 	
 	
 	
--- src/masProject/Supervisor.java
+++ src/masProject/Supervisor.java
@@ -1,16 +1,51 @@
 package masProject;
 
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 public class Supervisor  extends Agent{
 	
+	ArrayList<String> thesisProposal = new ArrayList<String>();
+	
+	public ArrayList<String> getThesisProposaList(){
+		
+		return thesisProposal;
+		
+	}
+	
+	
 	public void setup() {
+		
+		Object [] args = getArguments();
+		System.out.println("The arguments array has "+args.length);
+		
+		for(Object i: args) {
+			//System.out.println(" The value of i is"+ i);
+			thesisProposal.add((String) i);
+			
+		}
+		
+		//Array List 
+		for(String i:thesisProposal) {
+			//System.out.println("The value of i is aRRAY lIST  "+ i );
+		}
+		
 		
 		addBehaviour(new RequestsFromThesisCom());
 		addBehaviour(new MessageFromStudent());
+		addBehaviour(new StudentToSupervisor());
+		addBehaviour(new NoOfProposals());
+		//addBehaviour(new RecieveStudentThesisChoice());
 		
 		
 	}
@@ -61,6 +96,7 @@ public class Supervisor  extends Agent{
 			MessageTemplate mTemplate  = MessageTemplate.MatchConversationId("TChoice-Proposal"); 
 					//MatchPerformative(ACLMessage.INFORM);
 			ACLMessage AclMessage  = receive(mTemplate); 
+			ACLMessage no = receive(mTemplate);
 			
 			
 			if(AclMessage!=null) {
@@ -69,11 +105,13 @@ public class Supervisor  extends Agent{
 				System.out.println();
 				System.out.println("Message from Student to Supervisor ... ");
 				System.out.println(contentString);
-				
+			
+			
 				ACLMessage reply = AclMessage.createReply();
 				reply.setPerformative(ACLMessage.INFORM);
 				reply.setContent("THESIS INFORMATION");
 				myAgent.send(reply);
+					
 				
 			} else {
 				
@@ -85,13 +123,91 @@ public class Supervisor  extends Agent{
 		
 	}
 	
-	public class SendMoreInfoOnThePro extends CyclicBehaviour{
+	public class NoOfProposals extends OneShotBehaviour{
+		
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+	            int step = 0;
+					//System.out.println("Chose a company");
+					ACLMessage pr = new ACLMessage(ACLMessage.CFP); 
+					pr.addReceiver(new AID( "student",AID.ISLOCALNAME));
+					pr.setConversationId("numberOfThesis");
+					pr.setReplyWith("ThesisNum "+ System.currentTimeMillis());
+					
+					int size = thesisProposal.size();
+					String s=String.valueOf(size);
+				
+					pr.setContent(s);
+					send(pr);
+					
+					// Prepare a reply from the proposals 
+					MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId("numberOfThesis"), 
+							MessageTemplate.MatchInReplyTo(pr.getReplyWith()));
+					
+			
+					}
+		
+		}
+		
+		
+		
+
+	public class StudentToSupervisor extends CyclicBehaviour{
 
 		@Override
 		public void action() {
 			// TODO Auto-generated method stub
+			MessageTemplate mTemplate = MessageTemplate.MatchConversationId("TChoice-Proposal1");
+			ACLMessage msg = receive(mTemplate);
+			
+			if(msg!=null) {
+				String content = msg.getContent();
+				
+				System.out.println();
+				System.out.println("Reply from Student to Supervisor about Thesis decision ... ");
+				System.out.println(content);
+	            
+				String selected = "THESIS_SELECTED";
+				 //System.out.println(msg.toString());
+				 if(content.equals(selected) ) {
+					 Random random = new Random(); 
+					 int randomNo = random.nextInt((thesisProposal.size()-1)-0)+0;
+					 System.out.println(" THE SELECTED THESIS IS "+thesisProposal.get(randomNo));
+					 //System.out.println("THESIS REMOVED FROM SELECTION "+thesisProposal.remove(randomNo)); 
+					 
+				 }
+					
+			
+				//System.out.print(conf);
+				
+
+			}else {
+				
+				block();
+			}
 			
 			
+			
+		}
+		
+	}
+	
+	public class RecieveStudentThesisChoice extends CyclicBehaviour{
+
+		@Override
+		public void action() {
+			// TODO Auto-generated method stub
+			MessageTemplate mTemplate = MessageTemplate.MatchConversationId("chosenProposalNo");
+			ACLMessage msg = receive(mTemplate); 
+			
+			if(msg!=null) {
+				String content = msg.getContent(); 
+				System.out.println("Chosen Thesis is "+content); 
+				
+			}else {
+				block();
+			}
 		}
 		
 	}
